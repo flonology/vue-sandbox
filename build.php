ecccd8dc988f8e2cce9e5c118d54ffcd6eb2dfd6
Florian Hansen <hansen@mailbox.org>
2018-09-23 21:50:26 +0200

    Play around with Axios

--- build.php
+++ build.php
@@ -1,19 +1,26 @@
 #!/usr/bin/env php
 <?php
-$template = file_get_contents('index.tpl.html');
-$parts = scandir('parts');
+function build_index()
+{
+    $template = file_get_contents('index.tpl.html');
+    $parts = scandir('parts');
 
-foreach ($parts as $file) {
-    if ($file === '.')      continue;
-    if ($file === '..')     continue;
+    foreach ($parts as $file) {
+        if ($file === '.')      continue;
+        if ($file === '..')     continue;
 
-    echo "Processing {$file}…" . PHP_EOL;
+        echo "Processing {$file}…" . PHP_EOL;
 
-    $template = str_replace(
-        "[[{$file}]]",
-        file_get_contents("parts/{$file}"),
-        $template
-    );
+        $template = str_replace(
+            "[[{$file}]]",
+            file_get_contents("parts/{$file}"),
+            $template
+        );
+    }
+
+    $bytes_written = file_put_contents('index.html', $template);
+    return ceil($bytes_written / 1024);
 }
 
-file_put_contents('index.html', $template);
+$kb_written = build_index();
+echo "Wrote {$kb_written}kb." . PHP_EOL . PHP_EOL;
